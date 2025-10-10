03ffdc86b3a3da2064c64835886ceeb5d3fbf14a
Ty694nite <taiyebahmed694@gmail.com>
2025-10-10 16:13:51 +0100

    sum of 2 integers

---
+++ pract15.dart
@@ -0,0 +1,10 @@
+void main() {
+  // Example usage of the sum function
+  int result = sum(5, 7);
+  print('The sum is: $result');
+}
+
+// Function to return the sum of two integers
+int sum(int a, int b) {
+  return a + b;
+}
